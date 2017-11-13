6078903e84a15b801dfdae74990d4a0d3f73d73c
arturomf94 <arturomf94@gmail.com>
2017-11-13 21:40:39 +0000

    Change distribution of loan amounts

--- simulate.R
+++ simulate.R
@@ -14,12 +14,10 @@ set.seed(1994)
 #### Parameters ####
 
 loan_number = 4000
-max_amount = 250000
-min_amount = 40000
-mean_amount = 200000
-sd_amount = 40000
-mean_length = 18
-sd_length = 5
+max_amount = 300000
+min_amount = 10000
+mean_amount = 160000
+sd_amount = 70000
 
 # Initialize the data frame for the portfolio 
 portfolio = data.frame(1:loan_number)
@@ -30,9 +28,13 @@ colnames(portfolio) <- c('id')
 # Generate amounts with bootstrap function
 portfolio$amount <- rBootstrap(loan_number, mean_amount, sd_amount, min_amount, max_amount)
 
-# Round amounts to the nearest 5000
+# Round amounts to the nearest 5000 and clean invalid amounts
 
 portfolio$amount <- round(portfolio$amount / 5000,0) * 5000
+
+portfolio[portfolio$amount < 50000,]$amount <- 50000
+
+portfolio[portfolio$amount > 250000,]$amount <- 250000
 
 # Plot histogram and check stats
 
@@ -53,6 +55,4 @@ ggplot(data=portfolio, aes(portfolio$length)) +
   geom_histogram(binwidth = 1, col = 'darkolivegreen4', fill = 'darkolivegreen3') +
   ggtitle('Histogram of Loan Lengths') +
   labs(y = 'Count', x = 'Length (Months)')
-
-
 
