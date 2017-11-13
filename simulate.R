a90e19cfa82ce02a4e1d9679fe6adcbc1890260c
arturomf94 <arturomf94@gmail.com>
2017-11-13 21:31:17 +0000

    Add simulation of loan lengths and histogram

--- simulate.R
+++ simulate.R
@@ -18,6 +18,8 @@ max_amount = 250000
 min_amount = 40000
 mean_amount = 200000
 sd_amount = 40000
+mean_length = 18
+sd_length = 5
 
 # Initialize the data frame for the portfolio 
 portfolio = data.frame(1:loan_number)
@@ -26,7 +28,7 @@ colnames(portfolio) <- c('id')
 #### Simulate Loan Amounts ####
 
 # Generate amounts with bootstrap function
-portfolio$amount <- rBootstrap(loan_number, mean_amount, sd_amount, min_amount, 250000)
+portfolio$amount <- rBootstrap(loan_number, mean_amount, sd_amount, min_amount, max_amount)
 
 # Round amounts to the nearest 5000
 
@@ -40,4 +42,17 @@ ggplot(data=portfolio, aes(portfolio$amount)) +
   labs(y = 'Count', x = 'Amount')
 
 summary(portfolio$amount) 
+
+#### Simulate Loan Lengths ####
+
+portfolio$length <- sample(c(6,12,18), size = length(portfolio$id), replace=TRUE, prob=c(0.05,0.15,.8))
+
+# Plot histogram
+
+ggplot(data=portfolio, aes(portfolio$length)) + 
+  geom_histogram(binwidth = 1, col = 'darkolivegreen4', fill = 'darkolivegreen3') +
+  ggtitle('Histogram of Loan Lengths') +
+  labs(y = 'Count', x = 'Length (Months)')
+
+
 
